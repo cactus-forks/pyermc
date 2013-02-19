5fcfbc54418fcf800dd160b9a921820597a24021
elij <elij.mx@gmail.com>
2013-02-19 13:47:39 -0800

    more docstrings

--- pyermc/driver/__init__.py
+++ pyermc/driver/__init__.py
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Driver backends for pyermc
+"""
+
 from .base import Driver
 from .textproto import TextProtoDriver
 
